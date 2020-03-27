8bfb91c63057c372860c728ac1128f74eadbf7f1
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2020-03-27 11:36:25 -0300

    ADD deploy cluster scripts

---
+++ scripts/ocp3.11/ocp3.11uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openshiftAnsiblePath="/usr/share/ansible/openshift-ansible"
+inventoryPath=${OCP_USER_PATH}/playbooks/inventory.yaml
+
+ansible-playbook -i ${inventoryPath} ${openshiftAnsiblePath}/playbooks/adhoc/uninstall.yml
