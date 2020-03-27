cdfab6457e25bc3b5f5685f8c386645464c8fcf1
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2020-03-27 09:41:23 -0300

    ADD deploy cluster scripts

---
+++ scripts/ocp3.11/ocp3.11deploycluster.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openshift-ansible-path="/usr/share/ansible/openshift-ansible"
+inventory_path=${OCP_USER_PATH}/playbooks/inventory.yaml
+
+ansible-playbook -i ${inventory_path} ${openshift-ansible-path}/playbooks/deploy_cluster.yml
