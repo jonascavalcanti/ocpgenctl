fc801fb01533cde1e59e5bc5cb9c8b2253bb9b4f
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2020-05-19 12:05:34 -0300

    ADD user admin scripts

--- scripts/ocp4/update-user.sh
+++ scripts/ocp4/update-user.sh
@@ -4,11 +4,11 @@ USER=$1
 PASS=$2
 
 echo "Retrieve the HTPasswd file from the htpass-secret secret and save the file to your file system"
-oc get secret htpass-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 -d > users.htpasswd
+oc get secret htpass-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 -d > ${OCP_SHARED_FOLDER}/auth/users.htpasswd
 
 echo "To add or update a user"
-htpasswd -bB users.htpasswd $USER $PASS
+htpasswd -bB ${OCP_SHARED_FOLDER}/auth/users.htpasswd $USER $PASS
 
 echo "Replace the htpass-secret secret with the updated users in the users.htpasswd file"
-oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd --dry-run -o yaml -n openshift-config | oc replace -f -
+oc create secret generic htpass-secret --from-file=htpasswd=${OCP_SHARED_FOLDER}/auth/users.htpasswd --dry-run -o yaml -n openshift-config | oc replace -f -
 
