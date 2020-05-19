6f80e4ac227dba3a0354be6df07881dbed683f75
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2020-05-19 12:19:55 -0300

    ADD user admin scripts

---
+++ scripts/ocp4/oc-remove-user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USER=$1
+
+description(){
+    if [[ $# -eq 0 ]] ; then
+        echo $@ <username>
+        exit 1
+    fi
+}
+
+description
+
+echo "Retrieve the HTPasswd file from the htpass-secret secret and save the file to your file system"
+oc get secret htpass-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 -d > ${OCP_SHARED_FOLDER}/auth/users.htpasswd
+
+echo "Removing an existing $USER"
+htpasswd -D ${OCP_SHARED_FOLDER}/auth/users.htpasswd $USER
+
+echo "Deleting resources for the $USER"
+oc delete user $USER
+
+echo "Delete the identity for the $USER"
+oc delete identity my_htpasswd_provider:<username>
