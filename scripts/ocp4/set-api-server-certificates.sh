3589f6f40b34f7614f5454f0dfcb1c779c74bd3e
jonas.cavalcanti-goldentechnolog <jonas.cavalcanti@goldentechnologia.com.br>
2020-05-22 20:02:14 -0300

    Update

--- scripts/ocp4/set-api-server-certificates.sh
+++ scripts/ocp4/set-api-server-certificates.sh
@@ -6,50 +6,27 @@ CERT=$3
 CERT_KEY=$4
 HOSTNAME_CERT=$5
 
-if [[ $# -eq 4 ]] ; then
+if [[ $# -eq 5 ]] ; then
     echo "Using: $0 <comapny-name> <path-certificates> <cartificate-name> <key-cartificate-name> <hostname-certificate>" 
-    echo "Validation:
-            1. You must have the certificate and key, in the PEM format, for the client’s URL.
-
-            2. The certificate must be issued for the URL used by the client to reach the API server.
-
-            3. The certificate must have the subjectAltName extension for the URL.
-
-            4. If a certificate chain is required to certify the server certificate, 
-                then the certificate chain must be appended to the server certificate. 
-                Certificate files must be Base64 PEM-encoded and typically have a .crt or .pem extension. 
-                For example:
-
-            $ cat server_cert.pem int2ca_cert.pem int1ca_cert.pem rootca_cert.pem>combined_cert.pem
-
-            When combining certificates, the order of the certificates is important. 
-            Each following certificate must directly certify the certificate preceding it, 
-            for example:
-
-            * OpenShift Container Platform master host server certificate.
-
-            * Intermediate CA certificate that certifies the server certificate.
-
-            * Root CA certificate that certifies the intermediate CA certificate.
-        "
     exit 1
 fi
 
 echo "Create a secret that contains the certificate and key in the openshift-config namespace."
 oc create secret tls api-${COMPANY}-cert \
-     --cert=${PATH_CERTS}/${CERT}.crt> \
-     --key=${PATH_CERTS}/${CERT_KEY}.key> \
+     --cert=${PATH_CERTS}/${CERT} \
+     --key=${PATH_CERTS}/${CERT_KEY} \
      -n openshift-config
 
 echo "Update the API server to reference the created secret."
 oc patch apiserver cluster \
      --type=merge -p \
      '{"spec":{"servingCerts": {"namedCertificates":
-     [{"names": ["${HOSTNAME_CERT}"], 
-     "servingCertificate": {"name": "api-${COMPANY}-cert"}}]}}}'
+     [{"names": ["api.ocp.jdhlabs.com.br"], 
+     "servingCertificate": {"name": "api-jdhlabs-cert"}}]}}}'
+
+
 
 echo "Examine the apiserver/cluster object and confirm the secret is now referenced."
-
 echo "
     $ oc get apiserver cluster -o yaml
 ...
--- scripts/ocp4/set-ingress-certificate.sh
+++ scripts/ocp4/set-ingress-certificate.sh
@@ -5,24 +5,29 @@ PATH_CERTS=$2
 CERT=$3
 CERT_KEY=$4
 
-if [[ $# -eq 2 ]] ; then
+if [[ $# -eq 4 ]] ; then
     echo "Using: $0 <comapny-name> <path-certificates> <cartificate-name> <key-cartificate-name>"
     exit 1
 fi
 
 echo "Create a ConfigMap that includes the certificate authority used to signed the new certificate"
 oc create configmap custom-ca \
-     --from-file=ca-bundle.crt=${PATH_CERTS}/${CERT}.pem> \
+     --from-file=ca-bundle.crt=${PATH_CERTS}/${CERT} \
      -n openshift-config
 
 echo "Update the cluster-wide proxy configuration with the newly created ConfigMap"
+oc patch proxy/cluster \
+     --type=merge \
+     --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
+
+echo "Update the cluster-wide proxy configuration with the newly created ConfigMap"
 oc create secret tls apps-${COMPANY}-cert \
-     --cert=${PATH_CERTS}/${CERT}.pem \
-     --key=${PATH_CERTS}/${CERT_KEY}.key> \
+     --cert=${PATH_CERTS}/${CERT} \
+     --key=${PATH_CERTS}/${CERT_KEY} \
      -n openshift-ingress
 
 echo "Update the Ingress Controller configuration with the newly created secret:"
 oc patch ingresscontroller.operator default \
      --type=merge -p \
-     '{"spec":{"defaultCertificate": {"name": "apps-${COMPANY}-cert"}}}' \
+     '{"spec":{"defaultCertificate": {"name": "apps-jdhlabs-cert"}}}' \
      -n openshift-ingress-operator
