0a01088388467bebd9a8607d33af56f359fd2f04
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2020-06-05 22:47:58 -0300

    Update

--- scripts/ocp4/monitor-bootstrap-complete.sh
+++ scripts/ocp4/monitor-bootstrap-complete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${OCP_SHARED_FOLDER}/installers/openshift-install --dir=${OCP_SHARED_FOLDER}/ignitions/ wait-for bootstrap-complete --log-level=info
+${OCP_SHARED_FOLDER}/installers/openshift-install --dir=${OCP_SHARED_FOLDER}/ignitions/ wait-for bootstrap-complete --log-level=debug
--- scripts/ocp4/monitor-install-completion.sh
+++ scripts/ocp4/monitor-install-completion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${OCP_SHARED_FOLDER}/installers/openshift-install --dir=${OCP_SHARED_FOLDER}/ignitions/ wait-for install-complete --log-level=info
+${OCP_SHARED_FOLDER}/installers/openshift-install --dir=${OCP_SHARED_FOLDER}/ignitions/ wait-for install-complete --log-level=debug
